f39a660e5a8c46b6ccc305c5b299ebecb6c2f918
Hecer77 <mehdiyevah252@gmail.com>
2025-06-19 16:04:08 +0000

    4-print_float.py

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def print_float(number):
+number = -7
     if type(number) == float:
         print(f"Float: {number:.2f}")
     else:
         print("Wrong type")
 
-if __name__ == "__main__":
-    print_float(333.12)   # positive float
-    print_float(-45.678)  # negative float
-    print_float(0.0)      # zero float
-    print_float("hello")  # wrong type
