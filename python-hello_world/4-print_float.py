c9903a154c44e015cbc9e32c6827aeaaad4adf4f
Hecer77 <mehdiyevah252@gmail.com>
2025-06-19 16:13:19 +0000

    4-print_float.py

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -5,4 +5,7 @@ if type(number) == float:
 else:
     print("Wrong type")
 
-
+print_float(333.12)   # musbet
+print_float(-98.12)   # menfi
+print_float(0.0)      # sifirı
+print_float("hello")  # sehv tip
