d3d498f53a961172bb14f278391ca98bbc063d43
Hecer77 <mehdiyevah252@gmail.com>
2025-06-20 14:42:51 +0000

    7-edges.py

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 number = 3.14159
+<<<<<<< HEAD
 print(f"Float: {number:.2f}")
+=======
+
+if isinstance(number, float):
+    print(f"Float: {number:.2f}")
+else:
+    print("Wrong type")
+
+
+
+
+
+         Ä
+>>>>>>> 1f7f17c (Saved my local changes before pull)
