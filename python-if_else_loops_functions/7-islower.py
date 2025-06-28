471012ea51330ae4aa6511ae7d558e7ef5d65a4b
Hecer77 <mehdiyevah252@gmail.com>
2025-06-28 15:09:03 +0000

    7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -4,10 +4,3 @@ def islower(c):
     if not isinstance(c, str) or len(c) != 1:
         raise TypeError("Expected a single character string")
     return ord('a') <= ord(c) <= ord('z')
-
-
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
