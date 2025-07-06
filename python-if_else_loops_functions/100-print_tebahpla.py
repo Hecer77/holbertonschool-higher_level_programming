4a1c8646676b62beae8a4b5356884aa643286657
Hecer77 <mehdiyevah252@gmail.com>
2025-07-06 11:28:20 +0000

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+
+
 def print_reverse_alphabet():
-    print("".join("{}{}".format(chr(c), chr(c-1).upper()) for c in range(122, 96, -2)), end="")
+    print("".join(
+        "{}{}".format(chr(c), chr(c - 1).upper())
+        for c in range(122, 96, -2)
+    ), end="")
+
 
 print_reverse_alphabet()
+
