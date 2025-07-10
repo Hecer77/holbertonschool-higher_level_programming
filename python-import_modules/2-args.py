22cc330e9161f23d332b48446ce0eeec3864a781
Hecer77 <mehdiyevah252@gmail.com>
2025-07-10 13:43:01 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
     args = sys.argv[1:]
     count = len(args)
@@ -10,11 +11,10 @@ def main():
     elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:" .format(count))
-
+        print("{} arguments:".format(count))
 
     for i in range(count):
-        print("{}: {}" .format(i + 1, args[i])) 
+        print("{}: {}".format(i + 1, args[i]))
 
 
 if __name__ == "__main__":
