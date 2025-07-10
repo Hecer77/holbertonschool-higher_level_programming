5406e3c2a0362ece94dd6de1d8e04f41afd0b415
Hecer77 <mehdiyevah252@gmail.com>
2025-07-10 15:29:21 +0000

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
 def main():
     args = sys.argv[1:]
 
@@ -15,7 +14,6 @@ def main():
         total += int(arg)
 
     print(total)
-
 
 if __name__ == "__main__":
     main()
