77cb63b1dad41ff361a301aabba6b216ccc585d9
Hecer77 <mehdiyevah252@gmail.com>
2025-07-21 21:56:34 +0000

    10-main.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-
     best = None
     for key in a_dictionary:
         if best is None or a_dictionary[key] > a_dictionary[best]:
