58150a3d2a5c380702d951ed79a2f3048b001951
Hecer77 <mehdiyevah252@gmail.com>
2025-07-28 16:20:00 +0000

    6-raise_exception_msg.py

--- python-exceptions/main_1.py
+++ python-exceptions/main_1.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print("Python is cool")
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+try:
+    raise_exception_msg("Python is cool")
+except NameError as ne:
+    print(ne)
