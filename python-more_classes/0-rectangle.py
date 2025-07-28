39940a970b8657d27a5303385697cb78892c2d90
Hecer77 <mehdiyevah252@gmail.com>
2025-07-28 19:21:05 +0000

    0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+Bu modul Rectangle sinfini təyin edir.
+Sinif düzbucaqlı obyektləri genişlik və hündürlük ilə yaradır.
+"""
 class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         self.dict_ = {'width': self.width, 'height': self.height}
-
