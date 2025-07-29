f6479a68be497c00812090b64e51743b50512c28
Hecer77 <mehdiyevah252@gmail.com>
2025-07-29 14:26:46 +0000

    1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,16 +6,21 @@ və bu atributların doğruluğunu setter-lərlə yoxlayır.
 """
 
 class Rectangle:
-    def __init__(self, width= 0 , height= 0):
+    """Genişlik və hündürlüyə sahib düzbucaqlı sinfi"""
+
+    def __init__(self, width=0, height=0):
+        """Yeni düzbucaqlı obyektini yaradır"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Genişliyi qaytarır"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Genişliyi təyin edir, düzgünlüyü yoxlayır"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,12 +29,19 @@ class Rectangle:
 
     @property
     def height(self):
+        """Hündürlüyü qaytarır"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Hündürlüyü təyin edir, düzgünlüyü yoxlayır"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def dict(self):
+        """Obyektin atributlarını qaytarır"""
+        return {"width": self.__width, "height": self.__height}
